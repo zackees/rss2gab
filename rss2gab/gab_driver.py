61c3fe06c658c0dd20c23bc1f2811511712c8add
zackees <z@zackees.com>
2022-04-27 13:25:43 -0400

    adds image posting

--- rss2gab/gab_driver.py
+++ rss2gab/gab_driver.py
@@ -3,9 +3,14 @@
 """
 
 import ssl
+import time
+from typing import Optional
 
 from autoselenium import Driver  # type: ignore
 from selenium.webdriver.common.action_chains import ActionChains  # type: ignore
+from selenium.webdriver.common.keys import Keys  # type: ignore
+
+from .clipboard import clipboard_store_jpg
 
 ssl._create_default_https_context = (  # pylint: disable=protected-access
     ssl._create_unverified_context  # pylint: disable=protected-access
@@ -14,6 +19,8 @@ ssl._create_default_https_context = (  # pylint: disable=protected-access
 # Tested to work. For some reason the test fails if it's less resolution than this.
 WIDTH = 1200
 HEIGHT = 800
+
+TIMEOUT_IMAGE_UPLOAD = 60  # Wait upto 60 seconds to upload the image.
 
 
 def _action_login(driver: Driver, username: str, password: str) -> None:
@@ -30,7 +37,12 @@ def _action_login(driver: Driver, username: str, password: str) -> None:
     el_submit_btn.click()
 
 
-def _action_make_post(driver: Driver, content: str, dry_run: bool) -> None:
+def _action_make_post(
+    driver: Driver,
+    content: str,
+    jpg_path: Optional[str] = None,
+    dry_run: Optional[bool] = False,
+) -> None:
     """Makes a social media post"""
     driver.get("https://gab.com/compose")
     el_compose_window = driver.find_element_by_css_selector("div.DraftEditor-root")
@@ -39,6 +51,31 @@ def _action_make_post(driver: Driver, content: str, dry_run: bool) -> None:
     actions = ActionChains(driver)
     actions.send_keys(content)
     actions.perform()
+    # Upload the image if it's been specified.
+    if jpg_path is not None:
+        # Copy the image to the clipboard and then paste it into the post.
+        clipboard_store_jpg(jpg_path)
+        # Send a paste command to the keyboard.
+        actions = ActionChains(driver)
+        actions.key_down(Keys.META)
+        actions.send_keys("v")
+        actions.perform()
+        timeout = time.time() + TIMEOUT_IMAGE_UPLOAD
+        while True:
+            try:
+                # Wait for the image to upload.
+                # Find the element with the xpath that includes an image source
+                driver.find_element_by_xpath('//img[contains(@src, "media_attachments")]')
+                break
+            except Exception:  # pylint: disable=broad-except
+                if time.time() > timeout:
+                    print(
+                        f"{__file__}: Failed to upload image, because timed out waiting for "
+                        "{jpg_path} to upload"
+                    )
+                    break
+                time.sleep(0.5)
+    # Perform the post action.
     # Find an element that contains "Post"
     el_post_btn = driver.find_element_by_xpath('//*[contains(text(), "Post")]')
     # put the mouse over the button of el_post_btn and click it.
@@ -49,10 +86,16 @@ def _action_make_post(driver: Driver, content: str, dry_run: bool) -> None:
         actions.perform()
 
 
-def gab_post(username: str, password: str, content: str, dry_run: bool = False) -> None:
+def gab_post(
+    username: str,
+    password: str,
+    content: str,
+    jpg_path: Optional[str] = None,
+    dry_run: bool = False,
+) -> None:
     """Logs into Gab.com and posts the given content."""
     with Driver("firefox", root="drivers") as driver:
         driver.delete_all_cookies()
         driver.set_window_size(WIDTH, HEIGHT)
         _action_login(driver, username, password)
-        _action_make_post(driver, content, dry_run)
+        _action_make_post(driver, content, jpg_path=jpg_path, dry_run=dry_run)
--- tests/test_gabdriver.py
+++ tests/test_gabdriver.py
@@ -1,9 +1,15 @@
 """
     Tests the gab driver.
 """
+import os
 import unittest
 
 from rss2gab import gab_driver
+
+HERE = os.path.dirname(os.path.abspath(__file__))
+TEST_DATA = os.path.join(HERE, "data")
+SMALL_IMG = os.path.join(TEST_DATA, "small.jpg")
+
 
 USER = "testgabposter"
 PASS = "Yq4F2H9Lvp"
@@ -13,6 +19,10 @@ class GabDriverTest(unittest.TestCase):
     """Gab driver test framework."""
 
     def test_dryrun_posting(self) -> None:
+        """Tests that gab_post works, but doesn't not post."""
+        gab_driver.gab_post(USER, PASS, "test", jpg_path=SMALL_IMG, dry_run=True)
+
+    def test_dryrun_posting_with_image(self) -> None:
         """Tests that gab_post works, but doesn't not post."""
         gab_driver.gab_post(USER, PASS, "test", dry_run=True)
 
