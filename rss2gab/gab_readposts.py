52a5f14155df63d6eef71654ede9eff4fd24c5bf
zackees <z@zackees.com>
2022-04-30 17:41:31 -0400

    adds authentication to get gab posts

--- rss2gab/gab_readposts.py
+++ rss2gab/gab_readposts.py
@@ -8,11 +8,16 @@ from garc.client import Garc  # type: ignore
 DEFAULT_POST_LIMIT = 100
 
 
-def gab_readposts(user: str, limit: int = DEFAULT_POST_LIMIT) -> List[str]:
+def gab_readposts(
+    gab_id: str,
+    gab_login_user: str,
+    gab_login_pass: str,
+    limit: int = DEFAULT_POST_LIMIT,
+) -> List[str]:
     """Api to read the gab posts of the given user"""
-    # g = Garc(user, "")
-    gab = Garc()
-    posts = gab.userposts(user, gabs=limit)
+    gab = Garc(user_account=gab_login_user, user_password=gab_login_pass)
+    gab.login()
+    posts = gab.userposts(gab_id, gabs=limit)
     out = []
     for post in posts:
         body = post["body"]
@@ -25,7 +30,12 @@ def gab_readposts(user: str, limit: int = DEFAULT_POST_LIMIT) -> List[str]:
 
 def main() -> None:
     """Test runner"""
-    out = gab_readposts("perpetualmaniac", limit=1)
+    out = gab_readposts(
+        gab_id="testgabposter",
+        gab_login_user="testgabposter",
+        gab_login_pass="Yq4F2H9Lvp",
+        limit=1,
+    )
     print(out)
 
 
--- tests/test_garc.py
+++ tests/test_garc.py
@@ -6,15 +6,21 @@ import unittest
 
 from rss2gab.gab_readposts import gab_readposts
 
-ACCOUNT = "bigleaguepol"
+GAB_ACCOUNT_TO_FETCH = "bigleaguepol"
+GAB_LOGIN_USER = "testgabposter"
+GAB_LOGIN_PASS = "Yq4F2H9Lvp"
 
 
 class GarcTester(unittest.TestCase):
     """Gab driver test framework."""
 
     def test_fetch_bigleague(self) -> None:
-        """Tests that gab_post works, but doesn't not post."""
-        posts = gab_readposts(user=ACCOUNT)
+        """Tests that we can read bigleague politics from the test acccount."""
+        posts = gab_readposts(
+            gab_id=GAB_ACCOUNT_TO_FETCH,
+            gab_login_user=GAB_LOGIN_USER,
+            gab_login_pass=GAB_LOGIN_PASS,
+        )
         self.assertTrue(len(posts) > 0)
 
 
