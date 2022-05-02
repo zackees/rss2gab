c06f1388d22d87658954318550a0b43bb474b9c8
zackees <z@zackees.com>
2022-05-02 15:02:45 -0400

    updates app for client

--- apps/bigleague.py
+++ apps/bigleague.py
@@ -15,6 +15,12 @@ DRY_RUN = True  # Turn off when app is ready
 
 argparser = argparse.ArgumentParser()
 argparser.add_argument("--password", help="Password for gab.com account")
+argparser.add_argument(
+    "--published_hours_ago",
+    type=int,
+    help="How many hours ago to look for new posts from the rss feed.",
+    default=24,
+)
 args = argparser.parse_args()
 
 run(
@@ -22,5 +28,6 @@ run(
     gab_id=GAB_ID,
     gab_login_user=GAB_LOGIN_USER,
     gab_login_pass=args.password,
+    published_hours_ago=args.published_hours_ago,
     dry_run=DRY_RUN,
 )
